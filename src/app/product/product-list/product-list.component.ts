b74a5572de995f638f1a026d7674a3675010d62a
JossguerreroP <josguerrerop@udistrital.edu.co>
2024-07-31 00:11:33 -0500

    ok

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from '../../models/product';
+
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
+products: Product[]=[];
+
+  ngOnInit(): void {
+    this.prodctService.getProducts().subscribe(data => {
+      //this.products =data;
+      console.log(data)
+    })
+  }
+  constructor(private prodctService: ProductService){
+
+  }
 
 }
--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../models/product';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+private apiUrl = environment.apiUrl+"/products"
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  getProducts():Observable<Product[]>{
+    return this.http.get<Product[]>(this.apiUrl);
+  }
 }
