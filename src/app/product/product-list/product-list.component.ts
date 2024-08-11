afbd65109285b7c0cabe20e744c973ac9fc0e424
JossguerreroP <josguerrerop@udistrital.edu.co>
2024-08-10 21:28:56 -0500

    generic

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from '../../models/product';
 import { CartService } from '../../cart/cart.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-product-list',
@@ -19,14 +20,23 @@ products: Product[]=[];
   }
 
   addToCart(product: Product):void{
-    this.cartService.addToCart(product).subscribe();
+    this.cartService.addToCart(product).subscribe({
+      next: () => {
+        this.snackbar.open("Product added to cart!","",{
+          duration:2000,
+          horizontalPosition:'right',
+          verticalPosition:'top'
+        })
+      }
+
+    });
   }
 
 
 
   constructor(private prodctService: ProductService , 
-              private cartService: CartService){
-
+              private cartService: CartService,
+              private snackbar: MatSnackBar){
   }
 
 }
